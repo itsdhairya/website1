25af5d564d727bc15b6a22ffb4e0c8e155b7bce5
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-07 01:08:01 -0400

    update

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
-console.log('hello world');
+// console.log('hello world');
+app.get('/', function(req, res, next) { 
+    res.render('pages/home')
+});
+
+app.get('/about', (req, res,)=> { 
+    res.render('pages/about')
+});
 
 app.listen(3000);
