6e65a3e04f9c2958014904c6680434bcc6c0c2ae
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 16:27:18 -0400

    y

--- config/app.js
+++ config/app.js
@@ -1,10 +1,11 @@
+// Importing Modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
-var userRouter = require('../routes/user');
 
 // Instantiate Express
 var app = express();
@@ -13,16 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/style.css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
 
-
-// routes 
+// routes
 app.use('/', indexRouter);
-app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
