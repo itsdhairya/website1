eb2db3e6330554669c5646bcfb4deb00a59c53f9
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 18:51:53 -0400

    y

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var body_parser = require('body-parser');
-var app = express()
-const path = require('path');
+// var express = require('express');
+// var body_parser = require('body-parser');
+// var app = express()
+// const path = require('path');
 
-// Port config
-var port = 3000;
+// // Port config
+// var port = 3000;
 
-app.use(body_parser.json());
+// app.use(body_parser.json());
 
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '/views'));
+// app.set('view engine', 'ejs');
 
 
-app.listen(port, function(){
-  console.log('Server Active on', port);
-});
+// app.listen(port, function(){
+//   console.log('Server Active on', port);
+// });
 
-module.exports = app;
+// module.exports = app;
