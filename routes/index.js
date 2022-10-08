f01f5d6f8d1b884a9e5723be0548605f8446d96f
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 17:50:41 -0400

    y

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,98 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/index', home);
-router.get('/about-me', aboutme);
-router.get('/projects', projects);
-router.get('/services', services);
-router.get('/contact-me', contactme);
+// router.get('/index', home);
+// router.get('/about-me', aboutme);
+// router.get('/projects', projects);
+// router.get('/services', services);
+// router.get('/contact-me', contactme);
 
-function home(req, res, next) {
-    res.render('index', {});
-  }
+// function home(req, res, next) {
+//     res.render('/index', {});
+//   }
   
-  function aboutme(req, res, next) {
-    res.render('aboutme', {});
-  }
+//   function aboutme(req, res, next) {
+//     res.render('/about', {});
+//   }
   
-  function projects(req, res, next) {
-    res.render('project', {});
-  }
+//   function projects(req, res, next) {
+//     res.render('/project', {});
+//   }
   
-  function services(req, res, next) {
-    res.render('service', {});
-  }
+//   function services(req, res, next) {
+//     res.render('/service', {});
+//   }
   
-  function contactme(req, res, next) {
-    res.render('contactme', {});
-  }
+//   function contactme(req, res, next) {
+//     res.render('/contactme', {});
+//   }
   
-module.exports = router;
+// module.exports = router;
 
-// app.set('view engine', 'ejs');
+// router.set('view engine', 'ejs');
 
 // // console.log('hello world');
-// app.get('/', function(req, res, next) { 
-//     const student = {
-//         name: 'Dhairya Brahmbhatt',
-//         email: 'dhairya@gmail.com',
-//         description: "Inquisitive, energetic computer science specialist skilled in leadership with a strong foundation in math, logic, and cross-platform coding. I am seeking to leverage solid skills in collaboration, communication, and development as an administrative assistant.Passionate about analyzing service and product lines to maximize revenue. Have a B.A in Business Management. Skilled financial analysis, risk analysis, and pay great attention to detail."
-//     }
-//     res.render('pages/home' ,{students:student})
+// router.get('/', function(req, res, next) { 
+//     res.render('views/index' ,{students:student})
 // });
 
-// app.get('/about', (req, res,)=> { 
-//     res.render('pages/about')
+// router.get('/about', (req, res,)=> { 
+//     res.render('views/about')
 // });
 
-// app.get('/project', (req, res,)=> { 
-//     res.render('pages/project')
+// router.get('/project', (req, res,)=> { 
+//     res.render('views/project')
 // });
 
-// app.get('/service', (req, res,)=> { 
-//     res.render('pages/service')
+// router.get('/service', (req, res,)=> { 
+//     res.render('views/service')
 // });
 
-// module.exports = router
-// catch 404 errors and forward to error handlers
-// app.use(req, res, next) => {
-//     next(new createError.NotFound());
-// }
+// router.get('/contactme', (req, res,)=> { 
+//   res.render('views/contactme')
+// });
 
-// // error handler
-// app.use(err, req, res, next) => {
-//     //render the error message
-//     res.status(err.status || 500)
-//     res.render('error.html', {err});
-// }
 
 // app.listen(3000);
+
+// Importing Modules
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('../routes/index');
+
+var app = express();
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+
+// routes
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
