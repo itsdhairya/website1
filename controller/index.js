1d6b4c005959566a0bfd9f7c7b0713593d566960
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 14:47:14 -0400

    y

---
+++ controller/index.js
@@ -0,0 +1,27 @@
+exports.home = function(req, res, next) {
+    res.render('index', { 
+        title: 'Home',
+        userName: req.user ? req.user.username : ''
+    });
+};
+
+exports.aboutme = function(req, res, next) {
+    res.render('aboutme', { 
+        title: 'About Me',
+        userName: req.user ? req.user.username : ''
+     });
+}
+
+exports.projects = function(req, res, next) {
+    res.render('projects', { 
+      title: 'Projects',
+      userName: req.user ? req.user.username : '' 
+    });
+}
+
+exports.services = function(req, res, next) {
+    res.render('services', { 
+      title: 'Services',
+      userName: req.user ? req.user.username : '' 
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,29 @@ let app = express();
 let createError = require('http-errors');
 let router = express.Router();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-// console.log('hello world');
-app.get('/', function(req, res, next) { 
-    const student = {
-        name: 'Dhairya Brahmbhatt',
-        email: 'dhairya@gmail.com',
-        description: "Inquisitive, energetic computer science specialist skilled in leadership with a strong foundation in math, logic, and cross-platform coding. I am seeking to leverage solid skills in collaboration, communication, and development as an administrative assistant.Passionate about analyzing service and product lines to maximize revenue. Have a B.A in Business Management. Skilled financial analysis, risk analysis, and pay great attention to detail."
-    }
-    res.render('pages/home' ,{students:student})
-});
+// // console.log('hello world');
+// app.get('/', function(req, res, next) { 
+//     const student = {
+//         name: 'Dhairya Brahmbhatt',
+//         email: 'dhairya@gmail.com',
+//         description: "Inquisitive, energetic computer science specialist skilled in leadership with a strong foundation in math, logic, and cross-platform coding. I am seeking to leverage solid skills in collaboration, communication, and development as an administrative assistant.Passionate about analyzing service and product lines to maximize revenue. Have a B.A in Business Management. Skilled financial analysis, risk analysis, and pay great attention to detail."
+//     }
+//     res.render('pages/home' ,{students:student})
+// });
 
-app.get('/about', (req, res,)=> { 
-    res.render('pages/about')
-});
+// app.get('/about', (req, res,)=> { 
+//     res.render('pages/about')
+// });
 
-app.get('/project', (req, res,)=> { 
-    res.render('pages/project')
-});
+// app.get('/project', (req, res,)=> { 
+//     res.render('pages/project')
+// });
 
-app.get('/service', (req, res,)=> { 
-    res.render('pages/service')
-});
+// app.get('/service', (req, res,)=> { 
+//     res.render('pages/service')
+// });
 
 // module.exports = router
 // catch 404 errors and forward to error handlers
