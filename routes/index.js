654f3455569de32cc33ef59db60d2f9e7d45cf55
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 16:16:45 -0400

    y

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,32 @@
-let express = require('express');
-// let app = express();
-// let createError = require('http-errors');
-let router = express.Router();
-let controllerIndex = require('../controllers/index');
+var express = require('express');
+var router = express.Router();
 
+router.get('/', home);
+router.get('/about-me', aboutme);
+router.get('/projects', projects);
+router.get('/services', services);
+router.get('/contact-me', contactme);
 
-router.get('/', controllerIndex.home);
-router.get('/about-me', controllerIndex.aboutme);
-router.get('/projects', controllerIndex.projects);
-router.get('/services', controllerIndex.services);
-router.get('/contact-me', controllerIndex.contactme);
-
+function home(req, res, next) {
+    res.render('index', {});
+  }
+  
+  function aboutme(req, res, next) {
+    res.render('aboutme', {});
+  }
+  
+  function projects(req, res, next) {
+    res.render('projects', {});
+  }
+  
+  function services(req, res, next) {
+    res.render('services', {});
+  }
+  
+  function contactme(req, res, next) {
+    res.render('contactme', {});
+  }
+  
 module.exports = router;
 
 // app.set('view engine', 'ejs');
