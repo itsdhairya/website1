22297890875329eca0d3e5c270e687e75ae87fe0
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-08 20:15:54 -0400

    y

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// let createError = require('http-error');
-let cookieParser = require('cookie-parser');
+let createError = require("http-errors");
+let cookieParser = require("cookie-parser");
 // let logger = require('logger');
 // const app = require("./server");
-var express = require('express');
-let path = require('path');
-var port = 3000;
+var express = require("express");
+let path = require("path");
 
-let mainRoute = require('./routes/index')
+let mainRoute = require("./routes/index");
 
 var app = express();
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs'); 
+// app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
 // app.use(logger('dev'));
 app.use(express.json());
@@ -19,30 +18,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //for static files only
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "node_modules")));
 
 // routes
-app.use('/', mainRoute);
+app.use(mainRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',);
+  res.render("error");
 });
 
-app.listen(port, function(){
-  console.log('Server Active on', port);
-}); 
+app.listen(3000);
 
-module.exports = app;
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,13 @@
-
-// router.get('/index', home);
-// router.get('/about-me', aboutme);
-// router.get('/projects', projects);
-// router.get('/services', services);
-// router.get('/contact-me', contactme);
-
-// function home(req, res, next) {
-//     res.render('/index', {});
-//   }
-
-//   function aboutme(req, res, next) {
-//     res.render('/about', {});
-//   }
-
-//   function projects(req, res, next) {
-//     res.render('/project', {});
-//   }
-
-//   function services(req, res, next) {
-//     res.render('/service', {});
-//   }
-
-//   function contactme(req, res, next) {
-//     res.render('/contactme', {});
-//   }
-
-// module.exports = router;
-
-// router.set('view engine', 'ejs');
-
-// // console.log('hello world');
-// router.get('/', function(req, res, next) {
-//     res.render('views/index' ,{students:student})
-// });
-
 var express = require("express");
 var router = express.Router();
 
-let MainController = require('../controller/index');
+let MainController = require("../controller/index");
 
 router.get("/", MainController.home);
-
-router.get("views/index", MainController.home);
-
-router.get("views/about", MainController.about);
-
+router.get("/index", MainController.home);
+router.get("/about-me", MainController.about);
 router.get("/project", MainController.project);
-
 router.get("/service", MainController.service);
-
-router.get("/contactme", MainController.contactme);
-
-// router.get("/error", MainController.error);
+router.get("/contact-me", MainController.contact);
 
 module.exports = router;
